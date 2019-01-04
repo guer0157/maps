3ad13e9038521e48debae6ae3edd4a5df8cd188e
guer0157 <32390693+guer0157@users.noreply.github.com>
2019-01-03 22:42:00 -0500

    t
    t

--- js/mapwithArray.js
+++ js/mapwithArray.js
@@ -18,11 +18,23 @@ function initMap(){
     
     
     //////////////////////
+    var image = {
+    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32)
+  };
+ 
     
     //create array of markerts
     var markers=[
         {   props:{lat:45.3939,lng:-75.6831},
-            img:"../img/linkedin.png",
+            img:{url:"../img/linkedin.png",
+                scaledSize: new google.maps.Size(40, 40)
+            },
             content:"<h1>Tosca</h1>"
         },
         {
@@ -43,13 +55,16 @@ function initMap(){
         position:coords.props,
         map:map,
 //     this works but it's better to handle
-//undefined values before seeting the icon
-//        icon:coords.img
+//      undefined values before seeting the icon
+//      icon:coords.img
     });
     //This will handle any undefined img values.
         //Check for custom icon
         if(coords.img){
             marker.setIcon(coords.img);
+        }else{
+            console.log('custom marker')
+            marker.setIcon("../custom.png")
         }
        //This will handle any undefined content values.
         //Check for content   
